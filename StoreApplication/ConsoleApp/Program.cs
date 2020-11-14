52871759089ffeffdf7c3b37074639b3d5f87d87
Luke Fisher <thebigfishluke@gmail.com>
2020-11-14 15:51:27 -0500

    added getConnectionString and Logging implementation

--- StoreApplication/ConsoleApp/Program.cs
+++ StoreApplication/ConsoleApp/Program.cs
@@ -1,7 +1,12 @@
-﻿using StoreApplication.ClassLibrary.StoreApplication.Design;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using StoreApplication.ClassLibrary.StoreApplication.Design;
+using StoreApplication.DBClassLibrary;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 using System.Xml.Serialization;
 
 namespace ConsoleApp
@@ -10,54 +15,79 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            IApp app = new Application();
+            using var logStream = new StreamWriter("ef-logs.txt");
+
+            var optionsBuilder = new DbContextOptionsBuilder<Project0DBContext>();
+            optionsBuilder.UseSqlServer(getConnectionString());
+            optionsBuilder.LogTo(logStream.WriteLine, LogLevel.Information);
+
+            using var context = new Project0DBContext(optionsBuilder.Options);
+            //IApp app = new Application();
+
+            //try
+            //{
+            //    app.AddCustomer(new Customer("Luke", "Fisher"));
+
+            //}
+            //catch (ArgumentException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+
+            //Console.WriteLine("Welcome to my Store application! You must fill you inventory first");
+            //int UserInput = chooseInput();
+
+            //while (UserInput != 0)
+            //{
+            //    switch (UserInput)
+            //    {
+            //        case 1:
+            //            Console.Clear();
+            //            Console.WriteLine("Let's add a customer!");
+
+            //            string FirstName = "";
+            //            string LastName = "";
+
+
+            //            Console.WriteLine("What is the customer's first name?");
+            //            FirstName = Console.ReadLine();
+
+            //            Console.Clear();
+            //            Console.WriteLine("What is the customer's last name?");
+            //            LastName = Console.ReadLine();                       
+
+            //            app.AddCustomer(new Customer(FirstName, LastName));
+            //            Console.Clear();
+
+            //            Console.WriteLine("{0} {1} was added to Customers!", FirstName, LastName);
+
+            //            break;
+
+            //    }
+            //    UserInput = chooseInput();
+            //}
+
+
+        }
+
+        static string getConnectionString()
+        {
+            string path = "../../../DBConnectionString.json";
+            string json;
 
             try
             {
-                app.AddCustomer(new Customer("Luke", "Fisher"));
-
+                json = File.ReadAllText(path);
             }
-            catch (ArgumentException e)
+            catch (IOException)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("Path is not available");
+                throw;
             }
-
-
-            Console.WriteLine("Welcome to my Store application! You must fill you inventory first");
-            int UserInput = chooseInput();
-
-            while (UserInput != 0)
-            {
-                switch (UserInput)
-                {
-                    case 1:
-                        Console.Clear();
-                        Console.WriteLine("Let's add a customer!");
-
-                        string FirstName = "";
-                        string LastName = "";
-
-
-                        Console.WriteLine("What is the customer's first name?");
-                        FirstName = Console.ReadLine();
-
-                        Console.Clear();
-                        Console.WriteLine("What is the customer's last name?");
-                        LastName = Console.ReadLine();                       
-
-                        app.AddCustomer(new Customer(FirstName, LastName));
-                        Console.Clear();
-
-                        Console.WriteLine("{0} {1} was added to Customers!", FirstName, LastName);
-
-                        break;
-
-                }
-                UserInput = chooseInput();
-            }
-
-
-        }               
+            string connectionString = JsonSerializer.Deserialize<string>(json);
+            return connectionString;
+        }
 
         public static int chooseInput()
         {
